710c7b9782b15ffa961b60b8e3179ac82c644735
Prashant Gupta <68388206+PrashantGuptaa@users.noreply.github.com>
2020-11-08 21:55:19 +0530

    ns

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <h1 data-ns-test="project-name">IMDB of GAMES</h1>
+        <p data-ns-test="project-description">
+         All in one location for all games info
+        </p>
+      </>
+    );
+  }
 }
-
 
 export default App;
