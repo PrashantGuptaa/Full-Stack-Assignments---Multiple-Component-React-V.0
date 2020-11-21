a65a70ab97573743749a8151cdda5c6982d62818
Prashant Gupta <68388206+PrashantGuptaa@users.noreply.github.com>
2020-11-22 01:03:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,27 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
+import Project from "./Project";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <h1 data-ns-test="project-name">IMDB of GAMES</h1>
-        <p data-ns-test="project-description">
-         All in one location for all games info
-        </p>
-      </>
-    );
-  }
-}
+const App = () => {
+  const projects = [
+    {
+      heading: "Linkedin",
+      description:
+        "LinkedIn is an American business and employment-oriented online service that operates via websites and mobile apps. Launched on May 5, 2003, it is mainly used for professional networking, including employers posting jobs and job seekers posting their CVs."
+    },
+    {
+      heading: "Amazon",
+      description:
+        "Amazon.com, Inc. is an American multinational technology company based in Seattle, Washington, which focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence."
+    }
+  ];
+  return (
+    <>
+      {projects.map((item) => (
+        <Project heading={item.heading} description={item.description} />
+      ))}
+    </>
+  );
+};
 
 export default App;
